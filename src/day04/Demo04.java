31a3e7331799e409241dfd63d65dccc346a66ba8
taohu918 <hu_mails@163.com>
2018-08-04 03:27:22 +0800

    self

--- src/day04/Demo04.java
+++ src/day04/Demo04.java
@@ -9,6 +9,10 @@ class Demo4 {
         Benz b = new Benz();
         b.setColor("white");
         System.out.println(b.getColor());
+
+        Benz b3 = new Benz();
+        System.out.println(b3.getColor());
+        b3.outBrand();
     }
 }
 
@@ -20,11 +24,14 @@ class Benz {
     //构造代码块
     {
         //System.out.println(color);
+//        Benz.outBrand();
     }
 
     //静态方法,只能访问静态成员
     public static void outBrand() {
 //        System.out.println(this.tires);
+//        System.out.println(this.BRAND);
+        System.out.println(Benz.BRAND + "===");
     }
 
     public Benz() {
@@ -32,7 +39,7 @@ class Benz {
     }
 
     public Benz(String color) {
-        this();
+        this(); // 重用第一个构造函数
         this.color = color;
     }
 
@@ -43,10 +50,18 @@ class Benz {
 
     public String getColor() {
         System.out.println(BRAND);
+        System.out.println(Benz.BRAND);
+        System.out.println(this.BRAND);
         return color;
     }
 
-    public void setColor(String color) {
-        this.color = color;
+    public void setColor(String c) {
+        color = c;
+
     }
+
+//    public void setColor(String color) {
+//        color = color;
+//        this.color = color;
+//    }
 }
