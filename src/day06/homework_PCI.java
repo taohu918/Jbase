22dbd79c6e6b394ca521cca4b76a027bd0b159fa
hu_mails@163.com <hu_mails@163.com>
2018-09-13 22:46:51 +0800

    self

--- src/day06/homework_PCI.java
+++ src/day06/homework_PCI.java
@@ -2,19 +2,43 @@ package come.homework.pci;
 
 public class homework_PCI {
     public static void main(String args[]) {
-        Slot s = new Slot("PC");
+        VideoCard v = new VideoCard();
+        SoundCard s = new SoundCard();
+        NetworkCard n = new NetworkCard();
+
+        Computer c = new Computer();
+        c.slot(v);
+        c.slot(s);
+        c.slot(n);
 
     }
 }
 
-class Slot {
-    String name;
-
-    Slot(String name) {
-        this.name = name;
+class Computer {
+    public void slot(PCI pci) {
+        pci.play();
     }
+
 }
 
 interface PCI {
+    void play();
+}
 
+class VideoCard implements PCI {
+    public void play() {
+        System.out.println("video");
+    }
+}
+
+class SoundCard implements PCI {
+    public void play() {
+        System.out.println("sound");
+    }
+}
+
+class NetworkCard implements PCI {
+    public void play() {
+        System.out.println("network");
+    }
 }
